85b91ba341d6475165560cffc98af612c1fac798
Zbigniew Bernacki <zbernacki@aiut.com.pl>
2021-10-23 03:03:31 +0200

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from "react"
 import { Layout } from '../components/Layout/Layout'
-import { Header } from '../components/Header/Header'
 
 export const Home = () => {
   return (
     <Layout>
-      <Header />
 
     </Layout>
   )
