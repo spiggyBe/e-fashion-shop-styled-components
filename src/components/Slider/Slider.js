42f4e870dfb529354db9a4f9f6f5c924dc437255
Zbigniew Bernacki <zbernacki@aiut.com.pl>
2021-10-25 01:44:56 +0200

    slider content

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { SliderContainer, Arrow } from './SliderStyle'
+import { SliderContainer, Arrow, SliderTitle, StyledContent, SliderArticleContent, ActionButton } from './SliderStyle'
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
+
 
 export const Slider = () => {
     return (
@@ -12,6 +13,11 @@ export const Slider = () => {
             <Arrow rightArrow>
                 <KeyboardArrowRightIcon style={{ fontSize: '48px' }} />
             </Arrow>
+            <StyledContent>
+                <SliderTitle>You are welcome...</SliderTitle>
+                <SliderArticleContent>...if you want to change the world, first change yourself &amp; buy new clothes.</SliderArticleContent>
+                <ActionButton>Start&nbsp;&nbsp;journey</ActionButton>
+            </StyledContent>
         </SliderContainer>
     )
 }
